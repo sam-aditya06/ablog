661b9feabd30c4510191042e7e837294b6771a79
sam-aditya06 <48488126+sam-aditya06@users.noreply.github.com>
2024-04-21 10:16:01 +0530

    Update createToken.js

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -6,6 +6,7 @@ const createToken = (res, user) => {
 
     res.cookie('token', token, {
         httpOnly: true,
+        secure: true,
         sameSite: 'lax',
         maxAge: 30 * 24 * 60 * 60 * 1000
     });
